c2d6b7de6af75fccdef4c0bd34f0970c50160b3d
Thomas Schmidt <thomas.michel.schmidt@gmail.com>
2026-04-30 10:55:19 +0200

    INIT

---
+++ src/main/java/fr/schmidt/patiapi/PatiApiApplication.java
@@ -0,0 +1,13 @@
+package fr.schmidt.patiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/schmidt/patiapi/PatiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.schmidt.patiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
